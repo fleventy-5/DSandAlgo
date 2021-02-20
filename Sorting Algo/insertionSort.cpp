6434b1cee40bfb4f5017239200d5d9e08d6d3b01
Zera <you@example.com>
2021-02-20 19:31:50 +0530

    Added Insertion sort

---
+++ Sorting Algo/insertionSort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+void diaplay(int *a){
+
+	for(int j=0;j<6;j++){
+		printf("%d\t",*(a+j) );
+	}
+	cout<<endl;
+}
+
+int main(){
+	int a[6]={1,5,4,3,6,7},key,j;
+	for(int i=1 ;i<6;i++){
+		key = a[i];
+		j=i-1;
+		while(j>0 && a[j]>key){
+			a[j+1]=a[j];
+			j-=1;
+		}
+		a[j+1]=key;
+		
+	}
+	diaplay(a);
+	return 0;
+}
